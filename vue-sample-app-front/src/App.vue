dc492dadaf81646edaf36d304c172e797aada9e5
fromthegoldenwaves <juhyang0310@gmail.com>
2023-09-20 19:56:17 +0900

    delete layout and page
    delete some files which is not used

--- vue-sample-app-front/src/App.vue
+++ vue-sample-app-front/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <default></default>
-  <router-view />
+  <v-app>
+    <DefaultBar/>
+    <v-main>
+    <router-view />
+  </v-main>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-  import Default from './layouts/default/Default.vue';
+  // import Default from './layouts/default/Default.vue';
+  import DefaultBar from './layouts/default/AppBar.vue'
 </script>
