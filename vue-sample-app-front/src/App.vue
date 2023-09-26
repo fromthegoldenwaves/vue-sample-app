d0f76d0f1d5f20e8fb5d742fb407977d4d7ea51f
fromthegoldenwaves <juhyang0310@gmail.com>
2023-09-26 21:43:54 +0900

    change default view

--- vue-sample-app-front/src/App.vue
+++ vue-sample-app-front/src/App.vue
@@ -2,12 +2,11 @@
   <v-app>
     <DefaultBar/>
     <v-main>
-    <router-view />
-  </v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  // import Default from './layouts/default/Default.vue';
   import DefaultBar from './layouts/default/AppBar.vue'
 </script>
