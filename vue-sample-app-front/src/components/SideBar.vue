fc5b0a730dacbe2e3bbdde3c1c42485b83995dff
fromthegoldenwaves <juhyang0310@gmail.com>
2023-10-12 21:58:52 +0900

    Update SideBar.vue

--- vue-sample-app-front/src/components/SideBar.vue
+++ vue-sample-app-front/src/components/SideBar.vue
@@ -1,54 +1,44 @@
-<template>
-    <v-card>
-        <v-layout>
-            <v-navigation-drawer
-                v-model="drawer"
-                :rail="rail"
-                permanent
-                @click="rail = false"
-                
-            >
-                <v-list-item title = "TEST USER" nav>
-                    <template v-slot:append>
-                        <v-btn variant="text" icon="mdi-chevron-right" @click.stop="rail = !rail" ></v-btn>
-                    </template>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router';
 
-                    <v-divider></v-divider>
-                
-                    <v-list density="compact" nav>
-                        <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
-                        <v-list-item prepend-icon="mdi-note-text-outline" title="Plan" value="plan"></v-list-item>
-                        <v-list-item prepend-icon="mdi-cog" title="Setting" value="setting"></v-list-item>
-                        <v-list-item prepend-icon="mdi-help-circle" title="QnA" value="qna"></v-list-item>
-                        <v-list-item title="Log Out" value="logout" @click="backToMain"></v-list-item>
-                    </v-list>
-                </v-list-item>
-            </v-navigation-drawer>
-            <v-main style="height: 250px;"></v-main>
-        </v-layout>
-    </v-card>
+  const drawer = ref(true);
+  const rail = ref(true);
+
+  const router = useRouter();
+
+  function logout() {
+    router.push({ path: '/' });
+  }
+
+  function railToggle(){
+    rail.value = !rail.value;
+  }
+
+  function openRail() {
+    rail.value = false
+  }
+</script>
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="openRail"
+    >
+        <v-list-item title = "TEST USER" nav>
+            <v-divider/>
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-home" title="Main" value="main"></v-list-item>
+                <v-list-item prepend-icon="mdi-note-text-outline" title="Plan" value="plan"></v-list-item>
+                <v-list-item prepend-icon="mdi-cog" title="Setting" value="setting"></v-list-item>
+                <v-list-item prepend-icon="mdi-help-circle" title="QnA" value="qna"></v-list-item>
+                <v-list-item title="Log Out" value="logout" @click="logout"></v-list-item>
+            </v-list>
+            <template v-slot:append>
+                <v-btn variant="text" icon="mdi-chevron-right" @click.stop="railToggle" ></v-btn>
+            </template>
+        </v-list-item>
+    </v-navigation-drawer>
 </template>
 
-<script lang="ts">
-import { useRouter } from 'vue-router';
-    export default {
-        data () {
-            return {
-                drawer: true,
-                rail: true,
-            }
-        },
-
-        setup() {
-            const router = useRouter()
-
-            const backToMain = () => {
-                router.push({path: `/`})
-            }
-
-            return {
-                backToMain
-            }
-        }
-    }
-</script>
